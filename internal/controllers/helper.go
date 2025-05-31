0d554dc6728d2d7be9ebdaf9ee5e377fbbfb4e80
M. Fadhil Mahendra <mfmahendr@gmail.com>
2025-05-31 09:40:34 +0700

    feat(controllers): :sparkles: implement URL shortening and redirection functionality

---
+++ internal/controllers/helper.go
@@ -0,0 +1,22 @@
+package controllers
+import (
+	"errors"
+	"net/http"
+	"github.com/mfmahendr/url-shortener-backend/internal/utils/shortlink_errors"
+)
+
+func mapErrorToStatusCode(err error) (statusCode int) {
+	switch {
+	case errors.Is(err, shortlink_errors.ErrBlacklistedID):
+		statusCode = http.StatusForbidden
+	case errors.Is(err, shortlink_errors.ErrIDExists):
+		statusCode = http.StatusConflict
+	case errors.Is(err, shortlink_errors.ErrGenerateID), errors.Is(err, shortlink_errors.ErrSaveShortlink):
+		statusCode = http.StatusInternalServerError
+	case errors.Is(err, shortlink_errors.ErrValidateRequest):
+		statusCode = http.StatusUnprocessableEntity
+	default:
+		statusCode = http.StatusBadRequest
+	}
+	return statusCode
+}
