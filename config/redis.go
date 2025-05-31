16ca925de2222a4426136b3c84f4c93f08737e6c
M. Fadhil Mahendra <mfmahendr@gmail.com>
2025-05-31 12:06:07 +0700

    feat(config): :card_file_box: add NewRedisClient function for Redis connection

---
+++ config/redis.go
@@ -0,0 +1,15 @@
+package config
+
+import (
+	"os"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func NewRedisClient() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_ADDR"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       0,
+	})
+}
