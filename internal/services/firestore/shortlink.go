8a99525344d2ff87de0f814e5cbc3e53bc706410
M. Fadhil Mahendra <mfmahendr@gmail.com>
2025-05-31 22:48:58 +0700

    feat: :package: add and implement Shortlink model

--- internal/services/firestore/shortlink.go
+++ internal/services/firestore/shortlink.go
@@ -3,17 +3,18 @@ package firestore_service
 import (
 	"context"
 	"fmt"
-	
-	"cloud.google.com/go/firestore"
+
+	"github.com/mfmahendr/url-shortener-backend/internal/models"
+	"github.com/mfmahendr/url-shortener-backend/internal/utils/shortlink_errors"
 )
 
 type Shortlink interface {
 	DeleteShortlink(ctx context.Context, shortID string) error
-	GetShortlink(ctx context.Context, shortID string) (*firestore.DocumentSnapshot, error)
-	SetShortlink(ctx context.Context, shortID string, doc interface{}) error
+	GetShortlink(ctx context.Context, shortID string) (*models.Shortlink, error)
+	SetShortlink(ctx context.Context, shortID string, doc models.Shortlink) error
 }
 
-func (s *FirestoreServiceImpl) SetShortlink(ctx context.Context, shortID string, doc interface{}) error {
+func (s *FirestoreServiceImpl) SetShortlink(ctx context.Context, shortID string, doc models.Shortlink) error {
 	_, err := s.client.Collection("shortlinks").Doc(shortID).Set(ctx, doc)
 	if err != nil {
 		return fmt.Errorf("failed to set shortlink: %w", err)
@@ -21,18 +22,21 @@ func (s *FirestoreServiceImpl) SetShortlink(ctx context.Context, shortID string,
 	return nil
 }
 
-func (s *FirestoreServiceImpl) GetShortlink(ctx context.Context, shortID string) (*firestore.DocumentSnapshot, error) {
+// func (s *FirestoreServiceImpl) GetShortlink(ctx context.Context, shortID string) (*firestore.DocumentSnapshot, error) {
+func (s *FirestoreServiceImpl) GetShortlink(ctx context.Context, shortID string) (*models.Shortlink, error) {
 	docSnap, err := s.client.Collection("shortlinks").Doc(shortID).Get(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get shortlink: %w", err)
+	if !docSnap.Exists() {
+		return nil, shortlink_errors.ErrNotFound
 	}
-	return docSnap, nil
-}
 
-func (s *FirestoreServiceImpl) DeleteShortlink(ctx context.Context, shortID string) error {
-	_, err := s.client.Collection("shortlinks").Doc(shortID).Delete(ctx)
 	if err != nil {
-		return fmt.Errorf("failed to delete shortlink: %w", err)
+		return nil, shortlink_errors.ErrFailedRetrieveData
 	}
-	return nil
+
+	var shortlink models.Shortlink
+	if err := docSnap.DataTo(&shortlink); err != nil {
+		return nil, shortlink_errors.ErrFailedRetrieveData
+	}
+
+	return &shortlink, nil
 }
