098cd50c8c02901fc5683dc07f94914360d38110
M. Fadhil Mahendra <mfmahendr@gmail.com>
2025-05-31 10:10:20 +0700

    refactor(controllers): :fire: remove unused Controller and RouteRegistrar interfaces

--- internal/controllers/controller.go
+++ internal/controllers/controller.go
@@ -1,22 +1,13 @@
 package controllers
 
 import (
-	"net/http"
-
 	"github.com/julienschmidt/httprouter"
 	"github.com/mfmahendr/url-shortener-backend/internal/services/url_service"
 )
 
-type Controller interface {
-	Shorten(w http.ResponseWriter, r *http.Request, _ httprouter.Params)
-	Redirect(w http.ResponseWriter, r *http.Request, p httprouter.Params)
-	RouteRegistrar
-}
-
 type URLController struct {
 	Service url_service.URLService
 	Router  *httprouter.Router
-	Controller
 }
 
 func New(s url_service.URLService) *URLController {
