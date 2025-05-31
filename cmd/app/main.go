62415954bdc422324b7a42dc6b28aebd1dcda53c
M. Fadhil Mahendra <mfmahendr@gmail.com>
2025-05-31 10:12:21 +0700

    feat(main): :sparkles: refactor main function to use dependency injection for controller and auth middleware

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,28 +1,44 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/julienschmidt/httprouter"
-	"github.com/joho/godotenv"
+	"github.com/mfmahendr/url-shortener-backend/config"
+	"github.com/mfmahendr/url-shortener-backend/internal/di"
+	val "github.com/mfmahendr/url-shortener-backend/internal/utils/validators"
 )
 
 func main() {
-	// Load environment variables
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found")
+	if err := config.LoadEnv(); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
 	}
 
-	router := httprouter.New()
-	// routes.RegisterRoutes(router)
+	val.Init()
 
+	// initialize the controller and services using dependency injection
+	ctx := context.Background()
+	controller, err := di.InitializeController(ctx)
+	if err != nil {
+		log.Fatalf("failed to initialize app: %v", err)
+	}
+
+	// middleware and routes setup
+	authMiddleware, err := di.InitializeAuthMiddleware(ctx)
+	if err != nil {
+		log.Fatalf("failed to initialize auth middleware: %v", err)
+	}
+	controller.RegisterRoutes(*authMiddleware)
+
+
+	// start the HTTP server
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-	log.Printf("Server running at :%s", port)
-	log.Fatal(http.ListenAndServe(":"+port, router))
+	log.Printf("Server listening on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, controller.Router))
 }
