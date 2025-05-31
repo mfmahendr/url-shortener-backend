2f4a76815b295ea6a2ae7a7acb5444be0c2347c0
M. Fadhil Mahendra <mfmahendr@gmail.com>
2025-05-31 10:10:20 +0700

    refactor(controllers): :fire: remove unused Controller and RouteRegistrar interfaces

--- internal/controllers/route.go
+++ internal/controllers/route.go
@@ -7,10 +7,6 @@ import (
 )
 
 
-type RouteRegistrar interface {
-	RegisterRoutes(authMiddleware middleware.AuthMiddleware)
-}
-
 func (c *URLController) RegisterRoutes(authMiddleware middleware.AuthMiddleware) {
 	c.Router.Handle(http.MethodPost, "/shorten", authMiddleware.RequireAuth(c.Shorten))
 	c.Router.Handle(http.MethodGet, "/:short_id", c.Redirect)
