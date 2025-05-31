32807c75de9e15bfaf8a4303a63b2e8d6eb4f648
M. Fadhil Mahendra <mfmahendr@gmail.com>
2025-05-31 12:14:15 +0700

    refactor(firestoreservice): :recycle: decouple firestore and shortlink interfaces

--- internal/services/firestore/firestore.go
+++ internal/services/firestore/firestore.go
@@ -9,9 +9,6 @@ import (
 )
 
 type FirestoreService interface {
-	DeleteShortlink(ctx context.Context, shortID string) error
-	GetShortlink(ctx context.Context, shortID string) (*firestore.DocumentSnapshot, error)
-	SetShortlink(ctx context.Context, shortID string, doc interface{}) error
 	GetClient() *firestore.Client 
 }
 
@@ -19,7 +16,7 @@ type FirestoreServiceImpl struct {
 	client *firestore.Client
 }
 
-func New(ctx context.Context, firebaseApp *firebase.App) (FirestoreService, error) {
+func New(ctx context.Context, firebaseApp *firebase.App) (*FirestoreServiceImpl, error) {
 	client, err := firebaseApp.Firestore(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("failed to initialize firestore client: %w", err)
@@ -29,28 +26,4 @@ func New(ctx context.Context, firebaseApp *firebase.App) (FirestoreService, erro
 
 func (s *FirestoreServiceImpl) GetClient() *firestore.Client {
 	return s.client
-}
-
-func (s *FirestoreServiceImpl) SetShortlink(ctx context.Context, shortID string, doc interface{}) error {
-	_, err := s.client.Collection("shortlinks").Doc(shortID).Set(ctx, doc)
-	if err != nil {
-		return fmt.Errorf("failed to set shortlink: %w", err)
-	}
-	return nil
-}
-
-func (s *FirestoreServiceImpl) GetShortlink(ctx context.Context, shortID string) (*firestore.DocumentSnapshot, error) {
-	docSnap, err := s.client.Collection("shortlinks").Doc(shortID).Get(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get shortlink: %w", err)
-	}
-	return docSnap, nil
-}
-
-func (s *FirestoreServiceImpl) DeleteShortlink(ctx context.Context, shortID string) error {
-	_, err := s.client.Collection("shortlinks").Doc(shortID).Delete(ctx)
-	if err != nil {
-		return fmt.Errorf("failed to delete shortlink: %w", err)
-	}
-	return nil
 }
