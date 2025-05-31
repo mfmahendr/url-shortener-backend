a77860e9eaddd0382f08f5ece9c78e1ab85f7001
M. Fadhil Mahendra <mfmahendr@gmail.com>
2025-05-31 22:53:27 +0700

    refactor: :art: change struct names in shoten_dto.go and add ShortID field to ClickLog model

--- internal/models/click_log.go
+++ internal/models/click_log.go
@@ -3,6 +3,7 @@ package models
 import "time"
 
 type ClickLog struct {
+	ShortID    string    `json:"short_id,omitempty"`
 	Timestamp  time.Time `json:"timestamp"`
 	IP         string    `json:"ip"`
 	UserAgent  string    `json:"user_agent"`
