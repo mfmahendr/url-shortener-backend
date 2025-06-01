763515b24fa7afdde7ef03a75f241b3295f3a134
M. Fadhil Mahendra <mfmahendr@gmail.com>
2025-06-01 18:23:28 +0700

    fix(analytics): :label: change TotalClicks type and add Firestore tags to ClickLog fields
    change TotalClicks type from int to int64 for accurate click count and add Firestore tags to ClickLog fields for proper serialization

--- internal/dto/analytics_dto.go
+++ internal/dto/analytics_dto.go
@@ -4,6 +4,6 @@ import "github.com/mfmahendr/url-shortener-backend/internal/models"
 
 type AnalyticsDTO struct {
 	ShortID     string            `json:"short_id"`
-	TotalClicks int               `json:"total_clicks"`
+	TotalClicks int64             `json:"total_clicks"`
 	Clicks      []models.ClickLog `json:"clicks"`
 }
--- internal/models/click_log.go
+++ internal/models/click_log.go
@@ -3,8 +3,8 @@ package models
 import "time"
 
 type ClickLog struct {
-	ShortID    string    `json:"short_id,omitempty"`
-	Timestamp  time.Time `json:"timestamp"`
-	IP         string    `json:"ip"`
-	UserAgent  string    `json:"user_agent"`
+	ShortID    string    `json:"short_id,omitempty" firestore:"short_id"`
+	Timestamp  time.Time `json:"timestamp" firestore:"timestamp"`
+	IP         string    `json:"ip" firestore:"ip"`
+	UserAgent  string    `json:"user_agent" firestore:"user_agent"`
 }
