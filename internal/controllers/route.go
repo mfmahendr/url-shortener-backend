b744b69d259ab94e2dc9f803e44a5f625af65f4e
M. Fadhil Mahendra <mfmahendr@gmail.com>
2025-06-01 17:13:20 +0700

    fix(routes): :bug: correct URL path for click count and analytics endpoints

--- internal/controllers/route.go
+++ internal/controllers/route.go
@@ -8,7 +8,7 @@ func (c *URLController) RegisterRoutes(authMiddleware middleware.AuthMiddleware)
 	c.Router.POST("/shorten", authMiddleware.RequireAuth(c.Shorten))
 	c.Router.GET("/:short_id", c.Redirect)
 
-	c.Router.GET("/click-count/:short_id", authMiddleware.RequireAuth(c.GetClickCount))
-	c.Router.GET("/analytics/:short_id", authMiddleware.RequireAuth(c.Analytics))
+	c.Router.GET("/:short_id/click-count", authMiddleware.RequireAuth(c.GetClickCount))
+	c.Router.GET("/:short_id/analytics", authMiddleware.RequireAuth(c.Analytics))
 
 }
