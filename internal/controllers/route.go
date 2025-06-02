7fa7a255883e1bac9fa237a6b65c6228bc1fd4da
M. Fadhil Mahendra <mfmahendr@gmail.com>
2025-06-02 11:56:48 +0700

    feat: :stethoscope: add HealthCheck and Home endpoints for service status

--- internal/controllers/route.go
+++ internal/controllers/route.go
@@ -5,6 +5,9 @@ import (
 )
 
 func (c *URLController) RegisterRoutes(authMiddleware middleware.AuthMiddleware) {
+	c.Router.GET("/", c.Home)
+	c.Router.GET("/health", c.HealthCheck)
+
 	c.Router.POST("/shorten", authMiddleware.RequireAuth(c.Shorten))
 	c.Router.GET("/:short_id", c.Redirect)
 
