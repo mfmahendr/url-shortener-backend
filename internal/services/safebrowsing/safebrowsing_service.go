c96ac1c9478bc6905c961254ce89eac542968db6
M. Fadhil Mahendra <mfmahendr@gmail.com>
2025-06-13 06:42:25 +0700

    feat(blacklist): :zap: add Redis caching to SafeBrowsing checks

--- internal/services/safebrowsing/safebrowsing_service.go
+++ internal/services/safebrowsing/safebrowsing_service.go
@@ -2,9 +2,12 @@ package safebrowsing_service
 
 import (
 	"context"
+	"log"
+	"time"
 
 	"github.com/mfmahendr/url-shortener-backend/internal/utils/shortlink_errors"
 	"github.com/mfmahendr/url-shortener-backend/internal/utils/validators"
+	"github.com/redis/go-redis/v9"
 	"google.golang.org/api/option"
 	"google.golang.org/api/safebrowsing/v4"
 )
@@ -15,15 +18,17 @@ type URLSafetyChecker interface {
 
 type SafeBrowsingServiceImpl struct {
 	apiKey  string
-	Service *safebrowsing.Service
+	service *safebrowsing.Service
+	redis   *redis.Client
 }
 
-func New(ctx context.Context, apiKey string) URLSafetyChecker {
+func New(ctx context.Context, apiKey string, redis *redis.Client) URLSafetyChecker {
 	service, _ := safebrowsing.NewService(ctx, option.WithAPIKey(apiKey))
 
 	return &SafeBrowsingServiceImpl{
 		apiKey:  apiKey,
-		Service: service,
+		service: service,
+		redis: redis,
 	}
 }
 
@@ -32,6 +37,31 @@ func (s *SafeBrowsingServiceImpl) IsUnsafe(ctx context.Context, targetURL string
 		return false, shortlink_errors.ErrValidateRequest
 	}
 
+	// check cache
+	cacheKey := "safebrowsing:" + targetURL
+	cached, err := s.redis.Get(ctx, cacheKey).Result()
+	if err == nil {
+		log.Println("Check safebrowsing")
+		return cached == "unsafe", nil
+	}
+
+	// check API
+	unsafe, err := s.requestSafeBrowsingChecking(targetURL)
+	if err != nil {
+		return false, err
+	}
+	
+	// cache the URL for 24 hours
+	cacheVal := "safe"
+	if unsafe {
+		cacheVal = "unsafe"
+	}
+	_ = s.redis.Set(ctx, cacheKey, cacheVal, 24*time.Hour).Err()	// expires in 24 hours
+
+	return unsafe, nil
+}
+
+func (s *SafeBrowsingServiceImpl) requestSafeBrowsingChecking(targetURL string) (bool, error) {
 	req := &safebrowsing.GoogleSecuritySafebrowsingV4FindThreatMatchesRequest{
 		Client: &safebrowsing.GoogleSecuritySafebrowsingV4ClientInfo{
 			ClientId:      "url-shortener",
@@ -47,7 +77,7 @@ func (s *SafeBrowsingServiceImpl) IsUnsafe(ctx context.Context, targetURL string
 		},
 	}
 
-	response, err := s.Service.ThreatMatches.Find(req).Do()
+	response, err := s.service.ThreatMatches.Find(req).Do()
 	if err != nil {
 		return false, err
 	}
