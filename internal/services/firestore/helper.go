8ee493703f17f08cd58764d9cf1700f55b9692f0
M. Fadhil Mahendra <mfmahendr@gmail.com>
2025-06-12 15:01:47 +0700

    feat(clicklogs): :sparkles: add export functionality for click counts in CSV and JSON formats

--- internal/services/firestore/helper.go
+++ internal/services/firestore/helper.go
@@ -1,10 +1,15 @@
 package firestore_service
 
 import (
+	"context"
+	"fmt"
 	"time"
+
 	"cloud.google.com/go/firestore"
+	"google.golang.org/api/iterator"
 
 	"github.com/mfmahendr/url-shortener-backend/internal/dto"
+	"github.com/mfmahendr/url-shortener-backend/internal/utils/shortlink_errors"
 )
 
 
@@ -41,4 +46,15 @@ func (s *FirestoreServiceImpl) buildQuery(clickLogsQuery dto.ClickLogsQuery) (qu
 	query = query.Limit(clickLogsQuery.Limit)
 
 	return query
+}
+
+func checkDocumentExists(ctx context.Context, q firestore.Query) (bool, error) {
+	_, err := q.Limit(1).Documents(ctx).Next()
+	if err == iterator.Done {
+		return true, shortlink_errors.ErrNotFound
+	}
+	if err != nil {
+		return true, fmt.Errorf("failed to check documents: %w", err)
+	}
+	return false, nil
 }
