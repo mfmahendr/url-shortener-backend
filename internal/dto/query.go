59c634107d821715bc831d94541dcf4bd0f42c7f
M. Fadhil Mahendra <mfmahendr@gmail.com>
2025-06-12 14:58:30 +0700

    fix(dto): :bug: update validation rules for PaginationQuery and ClickLogsQuery

--- internal/dto/query.go
+++ internal/dto/query.go
@@ -5,7 +5,7 @@ import (
 )
 
 type PaginationQuery struct {
-	Limit     int    `json:"limit" validate:"required,min=1"`
+	Limit     int    `json:"limit" validate:"omitempty,min=1"`
 	Cursor    string `json:"cursor" validate:"omitempty"`
 	OrderDesc bool   `json:"order_desc" validate:"-"`
 }
@@ -13,7 +13,7 @@ type PaginationQuery struct {
 type ClickLogsQuery struct {
 	ShortID   string    `json:"short_id" validate:"required,short_id"`
 	UserAgent string    `json:"user_agent,omitempty" validate:"omitempty"`
-	After     time.Time `json:"after" validate:"omitempty,time"`
-	Before    time.Time `json:"before" validate:"omitempty,time"`
+	After     time.Time `json:"after" validate:"omitempty,datetime"`
+	Before    time.Time `json:"before" validate:"omitempty,datetime"`
 	PaginationQuery
 }
