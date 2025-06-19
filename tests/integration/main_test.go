944a6ade3a4eaf7124f2850177c5eb11b5aca4a4
M. Fadhil Mahendra <mfmahendr@gmail.com>
2025-06-19 22:26:37 +0700

    fix(tests): :truck: rename TestMain function file name and change GetSharedTestContainerEnv code
    Previously, TestMain didn't get executed because its filename didn't end in _test.go

--- tests/integration/main_test.go
+++ tests/integration/main_test.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 	"os"
 	"testing"
+
+	"github.com/mfmahendr/url-shortener-backend/internal/utils/validators"
 )
 
 func TestMain(m *testing.M) {
 	ctx := context.Background()
-
 	var err error
 	tcEnv, err = initializeTestContainerEnvironment(ctx)
 	if err != nil {
@@ -17,9 +18,14 @@ func TestMain(m *testing.M) {
 		os.Exit(1)
 	}
 
+	validators.Init()
+
 	exitCode := m.Run()
 
-	tcEnv.Cleanup(ctx)
+	err = tcEnv.Cleanup(ctx)
+	if err != nil {
+		fmt.Printf("failed to cleanup test env: %v\n", err)
+	}
 
 	os.Exit(exitCode)
 }
