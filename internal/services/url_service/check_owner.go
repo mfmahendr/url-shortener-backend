e757abc0dfa35f3491dae4dbc1e79ecf790ea407
M. Fadhil Mahendra <mfmahendr@gmail.com>
2025-06-19 22:36:18 +0700

    fix(urlservice): :necktie: add validation for shortID in IsOwner method

--- internal/services/url_service/check_owner.go
+++ internal/services/url_service/check_owner.go
@@ -2,9 +2,16 @@ package url_service
 
 import (
 	"context"
+
+	"github.com/mfmahendr/url-shortener-backend/internal/utils/shortlink_errors"
+	"github.com/mfmahendr/url-shortener-backend/internal/utils/validators"
 )
 
 func (s *URLServiceImpl) IsOwner(ctx context.Context, shortID string, uid string) (bool, error) {
+	if err := validators.Validate.Var(shortID, "short_id"); err != nil {
+		return false, shortlink_errors.ErrValidateRequest
+	}
+
 	shortlink, err := s.shortlink.GetShortlink(ctx, shortID)
 	if err != nil {
 		return false, err
