5be851354eaf08c2ec486ea8da2abb95c732d9d1
M. Fadhil Mahendra <mfmahendr@gmail.com>
2025-06-23 06:18:06 +0700

    fix(config): :bug: fix LoadEnv function to improve env variable loading logic

--- config/env.go
+++ config/env.go
@@ -2,14 +2,35 @@ package config
 
 import (
 	"errors"
+	"fmt"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() error {
-	if err := godotenv.Load(); err != nil {
-		return errors.New("No .env file found")
-	}
+	appMode := os.Getenv("APP_ENV")
 
+	switch appMode {
+	case "development":
+		if err := godotenv.Load(); err != nil {
+			return errors.New("no .env file found")
+		}
+		fallthrough
+	case "production":
+		return checkRequiredEnvVars()
+	default:
+		return errors.New("undefined app environment")
+	}
+}
+
+func checkRequiredEnvVars() error {
+	requiredVars := []string{"PORT", "GOOGLE_APPLICATION_CREDENTIALS", "FIREBASE_PROJECT_ID", "REDIS_ADDR", "REDIS_PASSWORD", "SAFE_BROWSING_API_KEY"}
+
+	for _, v := range requiredVars {
+		if os.Getenv(v) == "" {
+			return fmt.Errorf("missing required environment variable: %s", v)
+		}
+	}
 	return nil
 }
