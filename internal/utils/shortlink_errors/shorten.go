86efe4b23e130103d94910a89db8b6469de37c80
M. Fadhil Mahendra <mfmahendr@gmail.com>
2025-06-30 22:23:05 +0700

    fix: :goal_net: add ErrForbiddenInput for better error handling
    Previously, all errors due to the original URL input were considered as forbidden access

--- internal/utils/shortlink_errors/shorten.go
+++ internal/utils/shortlink_errors/shorten.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrSaveShortlink      = errors.New("failed to save short link")
 	ErrFailedRetrieveData = errors.New("failed to retrieve data from database")
+	ErrForbiddenInput     = errors.New("forbidden input")
 )
