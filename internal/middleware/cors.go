ac798bcc5e9cc2774026372e9e90092676f8fbee
M. Fadhil Mahendra <mfmahendr@gmail.com>
2025-11-04 16:36:26 +0700

    fix(hotfix/cors): :wheelchair: correct wrong env variables key from FRONTEND_URL to ALLOWED_ORIGINS

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -9,7 +9,7 @@ import (
 
 func CORS(router *httprouter.Router) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", os.Getenv("FRONTEND_URL"))
+		w.Header().Set("Access-Control-Allow-Origin", os.Getenv("ALLOWED_ORIGINS"))
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
